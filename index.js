c134808079142832a33d4312e3a98e1439f79ffd
hareeshvs72 <hareeshvs72@gmail.com>
2025-08-29 10:27:43 +0530

    json sever for resume

---
+++ index.js
@@ -0,0 +1,31 @@
+// import json server
+const JSONServer = require('json-server')
+
+// create server for running json file
+
+const  rBuilderServer  = JSONServer.create()
+
+// import db.json  
+
+const router = JSONServer.router("db.json")
+
+// create middleware
+ const middleware = JSONServer.defaults()
+
+// define port to run ther app
+
+const PORT = 3000 || process.env.PORT
+
+// use middleware json server
+ rBuilderServer.use(middleware)
+
+//  define routes for client request 
+
+rBuilderServer.use(router)
+
+// run server for client request
+
+rBuilderServer.listen(PORT,()=>{
+    console.log(`rbuilder server started at port : ${PORT} and waiting for client request !!!`);
+    
+})
